8d97c77b9a0b4c54de46ed7f30a953bdb4f98516
Andriy <andrijmaticak@gmail.com>
2020-06-24 14:16:03 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// let user_number = +prompt("Enter one number");
+
+// let user_number_2 = +prompt("Enter second number");
+
+// function numberResult(){
+// 	if (user_number > user_number_2) {
+// 		alert("First number is greater than the second  ");
+// 	}
+
+// 	else if (user_number < user_number_2) {
+// 	alert("Second number is greater than the first  ");
+
+// 	}
+
+// 	else {
+// 		alert("error");
+// 	}
+// }
+
+// numberResult();
+
+//     2
+
+// function numberUserResult(){
+// 	let user_number = prompt("Enter number!");
+
+// 	let stepin = prompt("Enter stepin!");
+
+// 	let result = alert(user_number ** stepin);
+// }
+
+
+// numberUserResult();
+
+// Все працює , якщо є проблеми пишіть, і хотів запитати мені використовувать глобальну перемінну чи локальну?Чи в загалі немає різниці?
+
